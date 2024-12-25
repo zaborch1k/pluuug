0d7fc851f2a0af8f699e74826e853304521cfc68
zaborch1k <156689182+zaborch1k@users.noreply.github.com>
2024-12-25 11:04:10 +0300

    Delete bin directory

--- bin/ext_wdw.js
+++
@@ -1,9 +0,0 @@
-var flagExtWdw = false;
-
-function deactivate() {
-    window.location.href = "../windows/main.html";
-}
-
-function openExtWindow() {
-    chrome.tabs.create({url : chrome.runtime.getURL("../windows/ext_wdw.html")});
-}
